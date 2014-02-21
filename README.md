b0bf8189a1bb55728e57f5495917f944fe87cad7
Tyler Sommer <sommertm@gmail.com>
2014-02-21 13:39:49 -0800

    Added unit tests

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 nice
 ====
 
-A nice PHP microframework
+A nice PHP microframework.
+
+This framework integrates nikic's FastRoute router with the Symfony 2 HttpKernel.
+
+Further improvements to come:
+
+* Some kind of dependency injection solution
+* Production/development environments
+* Caching of some kind
 
 
 Installation
---
+++ tests/Router/RouterSubscriberTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace TylerSommer\Nice\Tests\Router;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use TylerSommer\Nice\Router\RouterSubscriber;
+
+class RouterSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test a Found route
+     */
+    public function testFound()
+    {
+        $subscriber = $this->getSubscriber();
+        $request = Request::create('/', 'GET');
+        
+        $event = $this->getEvent($request);
+        
+        $subscriber->onKernelRequest($event);
+        
+        $this->assertEquals('handler1', $request->get('_controller'));
+    }
+
+    /**
+     * Test a match, but Method Not Allowed route
+     */
+    public function testMethodNotAllowed()
+    {
+        $subscriber = $this->getSubscriber();
+        $request = Request::create('/', 'POST');
+
+        $event = $this->getEvent($request);
+
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException');
+        
+        $subscriber->onKernelRequest($event);
+    }
+
+    /**
+     * Test not found
+     */
+    public function testNotFound()
+    {
+        $subscriber = $this->getSubscriber();
+        $request = Request::create('/not-real', 'GET');
+
+        $event = $this->getEvent($request);
+
+        $this->setExpectedException('Symfony\Component\HttpKernel\Exception\NotFoundHttpException');
+
+        $subscriber->onKernelRequest($event);
+    }
+
+    /**
+     * @return RouterSubscriber
+     */
+    private function getSubscriber()
+    {
+        $routeDispatcher = \FastRoute\simpleDispatcher(function(\FastRoute\RouteCollector $r) {
+                $r->addRoute('GET', '/', 'handler1');
+            });
+        
+        $subscriber = new RouterSubscriber($routeDispatcher);
+        
+        return $subscriber;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return GetResponseEvent
+     */
+    private function getEvent(Request $request)
+    {
+        return new GetResponseEvent(
+            $this->getMockForAbstractClass('Symfony\Component\HttpKernel\HttpKernelInterface'), 
+            $request, 
+            HttpKernelInterface::MASTER_REQUEST
+        );
+    }
+}
