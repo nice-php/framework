9e5f975f32a5ee1dd3ec80ecdcf5a3579a18f9dd
Tyler Sommer <sommertm@gmail.com>
2014-02-21 13:18:44 -0800

    Added RouteSubscriber

---
+++ src/Router/RouterSubscriber.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace TylerSommer\Nice\Router;
+
+use FastRoute\Dispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RouterSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var \FastRoute\Dispatcher
+     */
+    private $dispatcher;
+
+    /**
+     * Constructor
+     * 
+     * @param Dispatcher $dispatcher
+     */
+    public function __construct(Dispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $routeInfo = $this->dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
+        switch ($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                $message = sprintf('No route found for "%s %s"', $request->getMethod(), $request->getPathInfo());
+
+                throw new NotFoundHttpException($message);
+
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                $message = sprintf('No route found for "%s %s": Method Not Allowed (Allow: %s)', $request->getMethod(), $request->getPathInfo(), implode(', ', $allowedMethods));
+
+                throw new MethodNotAllowedHttpException($allowedMethods, $message);
+
+            case Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+
+                $request->attributes->set('_route_params', $vars);
+                $request->attributes->set('_controller', $handler);
+
+                break;
+        }
+    }
+    
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array('kernel.request' => 'onKernelRequest');
+    }
+}
