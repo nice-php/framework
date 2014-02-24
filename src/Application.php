fb32d3bb8c9a2cc1baf5bf25aa5c94726b89c2ac
Tyler Sommer <sommertm@gmail.com>
2014-02-24 12:22:23 -0800

    Initial integration with symfony dic

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,10 @@
 
 namespace TylerSommer\Nice;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,33 +18,59 @@ use TylerSommer\Nice\Router\RouterSubscriber;
 class Application extends HttpKernel
 {
     /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
      * Constructor
-     * 
-     * @param callable                    $routeFactory
+     *
+     * @param ContainerInterface          $container
      * @param EventDispatcherInterface    $dispatcher
      * @param ControllerResolverInterface $resolver
      * @param RequestStack                $requestStack
+     *
+     * @internal param callable $routeFactory
      */
     public function __construct(
-        callable $routeFactory,
+        ContainerInterface $container = null,
         EventDispatcherInterface $dispatcher = null,
         ControllerResolverInterface $resolver = null,
         RequestStack $requestStack = null
     ) {
-        $dispatcher = $dispatcher ?: new EventDispatcher();
+        $this->container = $container = $container ?: new ContainerBuilder();
+
+        $dispatcher = $dispatcher ?: new ContainerAwareEventDispatcher($container);
         $resolver   = $resolver ?: new ControllerResolver();
 
         parent::__construct($dispatcher, $resolver, $requestStack);
 
-        $routeDispatcher = \FastRoute\simpleDispatcher($routeFactory);
-        $subscriber = new RouterSubscriber($routeDispatcher);
+        $container->register('router.parser', 'FastRoute\RouteParser\Std');
+        $container->register('router.data_generator', 'FastRoute\DataGenerator\GroupCountBased');
+        $container->register('router.collector', 'FastRoute\RouteCollector')
+            ->addArgument(new Reference('router.parser'))
+            ->addArgument(new Reference('router.data_generator'));
 
-        $dispatcher->addSubscriber($subscriber);
+        $container->register('routes', 'Closure')
+            ->setSynthetic(true);
+
+        $container->register('router.dispatcher_factory', 'TylerSommer\Nice\Router\DispatcherFactory\GroupCountBasedFactory')
+            ->addArgument(new Reference('router.collector'))
+            ->addArgument(new Reference('routes'));
+
+        $container->register('router.dispatcher', 'FastRoute\Dispatcher')
+            ->setFactoryService('router.dispatcher_factory')
+            ->setFactoryMethod('create');
+
+        $container->register('router.dispatcher_subscriber', 'TylerSommer\Nice\Router\RouterSubscriber')
+            ->addArgument(new Reference('router.dispatcher'));
+
+        $dispatcher->addSubscriberService('router.dispatcher_subscriber', 'TylerSommer\Nice\Router\RouterSubscriber');
     }
 
     /**
      * Helper method to get things going.
-     * 
+     *
      * Inspired by Silex
      */
     public function run()
@@ -49,5 +79,13 @@ class Application extends HttpKernel
         $response = $this->handle($request);
         $response->send();
         $this->terminate($request, $response);
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
     }
 }
