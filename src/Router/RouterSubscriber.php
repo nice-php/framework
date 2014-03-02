8d507eb3e64da02dfa9f223ab1a8e02556c8de3d
Tyler Sommer <sommertm@gmail.com>
2014-03-01 17:08:21 -0800

    Added license blurb

--- src/Router/RouterSubscriber.php
+++ src/Router/RouterSubscriber.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Nice\Router;
 
 use FastRoute\Dispatcher;
