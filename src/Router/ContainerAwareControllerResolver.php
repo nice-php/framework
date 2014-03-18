5e3b2641659177a2b316660dea1f1b53452e0db0
Tyler Sommer <sommertm@gmail.com>
2014-03-18 08:46:09 -0700

    Added ContainerAwareControllerResolver and tests

---
+++ src/Router/ContainerAwareControllerResolver.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Nice\Router;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
+/**
+ * Extends the basic ControllerResolver to support ContainerAware controllers
+ */
+class ContainerAwareControllerResolver extends ControllerResolver implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     *
+     * @api
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Returns a callable for the given controller.
+     *
+     * @param string $controller A Controller string
+     *
+     * @return mixed A PHP callable
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function createController($controller)
+    {
+        $controller = parent::createController($controller);
+        
+        if ($controller[0] instanceof ContainerAwareInterface) {
+            $controller[0]->setContainer($this->container);
+        }
+        
+        return $controller;            
+    }
+}
---
+++ tests/Router/ContainerAwareControllerResolverTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Nice\Tests\Router;
+
+use Nice\Router\ContainerAwareControllerResolver;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContainerAwareControllerResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that the ContainerAwareControllerResolver properly injects the container
+     */
+    public function testInjectsContainer()
+    {
+        $request = new Request();
+        $request->attributes->set('_controller', 'Nice\Tests\Router\ContainerAwareController::someAction');
+        $resolver = new ContainerAwareControllerResolver();
+        
+        $container = new Container();
+        $resolver->setContainer($container);
+        
+        $controller = $resolver->getController($request);
+        
+        $this->assertCount(2, $controller);
+        $this->assertSame($container, $controller[0]->getContainer());
+    }
+}
+
+class ContainerAwareController implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     *
+     * @api
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
+    
+    /**
+     * An action
+     */
+    public function someAction()
+    {
+        // no op
+    }
+}
