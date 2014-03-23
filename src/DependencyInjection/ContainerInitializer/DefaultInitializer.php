1f04ce9a01aaf53fd2546b0ac8c4b79db172adc0
Tyler Sommer <sommertm@gmail.com>
2014-03-23 13:30:55 -0700

    Extracted container initialization into ContainerInitializer classes

---
+++ src/DependencyInjection/ContainerInitializer/DefaultInitializer.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Nice\DependencyInjection\ContainerInitializer;
+
+use Nice\Application;
+use Nice\DependencyInjection\ContainerInitializerInterface;
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\MergeExtensionConfigurationPass;
+use Symfony\Component\HttpKernel\DependencyInjection\RegisterListenersPass;
+
+class DefaultInitializer implements ContainerInitializerInterface
+{
+    /**
+     * @var string
+     */
+    private $environment;
+
+    /**
+     * @var bool
+     */
+    private $debug;
+
+    /**
+     * Constructor
+     * 
+     * @param string $environment
+     * @param bool   $debug
+     */
+    public function __construct($environment, $debug = false)
+    {
+        $this->environment = $environment;
+        $this->debug       = $debug;
+    }
+
+    /**
+     * Returns a fully built, ready to use Container
+     *
+     * @param Application $application
+     *
+     * @return ContainerInterface
+     */
+    public function initializeContainer(Application $application)
+    {
+        $container = $container = $this->getContainerBuilder();
+        $container->addObjectResource($application);
+        $container->setParameter('app.env', $application->getEnvironment());
+        $container->setParameter('app.debug', $application->isDebug());
+
+        $container->register('event_dispatcher', 'Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher')
+            ->setArguments(array(new Reference('service_container')));
+
+        $container->register('app', 'Symfony\Component\HttpKernel\HttpKernelInterface')
+            ->setSynthetic(true);
+        
+        $container->register('request', 'Symfony\Componenet\HttpKernel\Request')
+            ->setSynthetic(true);
+
+        $extensions = array();
+        foreach ($application->getRegisteredExtensions() as $extension) {
+            $container->registerExtension($extension);
+            $extensions[] = $extension->getAlias();
+        }
+
+        $container->addCompilerPass(new MergeExtensionConfigurationPass($extensions));
+        $container->addCompilerPass(new RegisterListenersPass());
+
+        $container->compile();
+        
+        return $container;
+    }
+    
+    /**
+     * Gets a new ContainerBuilder instance used to build the service container.
+     *
+     * @return ContainerBuilder
+     */
+    protected function getContainerBuilder()
+    {
+        return new ContainerBuilder();
+    }
+}
