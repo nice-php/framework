e6595e2d5068907ae362d11f25c07aff4072a21c
Tyler Sommer <sommertm@gmail.com>
2014-03-26 16:08:41 -0700

    Added RouteCollectorInterface extracted from FastRoute

---
+++ src/Router/RouteCollectorInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Copyright (c) Tyler Sommer
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Nice\Router;
+
+/**
+ * Defines the contract any RouteCollector must implement
+ * 
+ * This interface is extracted from FastRoute\RouteCollector
+ */
+interface RouteCollectorInterface
+{
+    /**
+     * Returns the collected route data
+     *
+     * @return array
+     */
+    public function getData();
+}
