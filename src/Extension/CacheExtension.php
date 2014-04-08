f410c94885ce007e12fbbdf955ae3db8d89b6f22
Tyler Sommer <sommertm@gmail.com>
2014-04-08 09:55:04 -0600

    Furthered cache extension implementation, added support for memcache(d) and array caches

--- src/Extension/CacheExtension.php
+++ src/Extension/CacheExtension.php
@@ -62,19 +62,110 @@ class CacheExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
         
-        foreach ($config['connections'] as $index => $connection) {
-            $name = $connection['name'];
-            
-            $driverDefinition = $container->register('cache.' . $name . '.driver');
-            $driverDefinition->setClass('Redis');
-            $driverDefinition->addMethodCall('pconnect', array(
-                    '/tmp/redis.sock'
-                ));
-            
-            $definition = $container->register('cache.' . $name);
-            $definition->setClass('Doctrine\Common\Cache\RedisCache');
-            $definition->addMethodCall('setRedis', array(new Reference('cache.' . $name . '.driver')));
-            $definition->addMethodCall('setNamespace', array($connection['namespace']));
+        foreach ($config['connections'] as $cacheConfig) {
+            switch ($cacheConfig['driver']) {
+                case 'redis':
+                    $this->configureRedisCache($cacheConfig, $container);
+                    
+                    break;
+                
+                case 'memcached':
+                case 'memcache':
+                    $this->configureMemcachedCache($cacheConfig, $container);
+                    
+                    break;
+                    
+                case 'array':
+                    $this->configureArrayCache($cacheConfig, $container);
+                    
+                    break;
+                    
+                default:
+                    throw new \RuntimeException(sprintf('The driver "%s" is not supported.', $cacheConfig['driver']));
+            }
         }
+    }
+    
+    private function configureRedisCache(array $cacheConfig, ContainerBuilder $container)
+    {
+        $defaults = array(
+            'host' => '127.0.0.1',
+            'socket' => null,
+            'port' => 6379,
+            'database' => 0,
+            'persistent' => true,
+            'timeout' => 0
+        );
+        
+        $options = array_merge($defaults, $cacheConfig['options']);
+        if (isset($options['socket'])) {
+            $options['host'] = $options['socket'];
+            $options['port'] = null;
+        }
+
+        $name = $cacheConfig['name'];
+        $cacheService = 'cache.' . $name;
+        $driverService = $cacheService . '.driver';
+
+        $driverDefinition = $container->register($driverService);
+        $driverDefinition->setClass('Redis');
+        $driverDefinition->addMethodCall($options['persistent'] ? 'pconnect' : 'connect', array(
+                $options['host'],
+                $options['port'],
+                $options['timeout']
+            ));
+
+        $definition = $container->register($cacheService);
+        $definition->setClass('Doctrine\Common\Cache\RedisCache');
+        $definition->addMethodCall('setRedis', array(new Reference($driverService)));
+        $definition->addMethodCall('setNamespace', array($cacheConfig['namespace']));
+    }
+
+    private function configureMemcachedCache(array $cacheConfig, ContainerBuilder $container)
+    {
+        $defaults = array(
+            'host' => '127.0.0.1',
+            'socket' => null,
+            'port' => 11211,
+        );
+
+        $options = array_merge($defaults, $cacheConfig['options']);
+        if (isset($options['socket'])) {
+            $options['host'] = $options['socket'];
+            $options['port'] = null;
+        }
+
+        $cacheService = 'cache.' . $cacheConfig['name'];
+        $driverService = $cacheService . '.driver';
+        
+        $driverDefinition = $container->register($driverService);
+        $driverDefinition->addMethodCall('addServer', array(
+                $options['host'],
+                $options['port']
+            ));
+
+        $cacheDefinition = $container->register($cacheService);
+        $cacheDefinition->addMethodCall('setNamespace', array($cacheConfig['namespace']));
+        
+        if ($cacheConfig['driver'] === 'memcache') {
+            $driverDefinition->setClass('Memcache');
+            $cacheDefinition->setClass('Doctrine\Common\Cache\MemcacheCache');
+            $cacheDefinition->addMethodCall('setMemcache', array(new Reference($driverService)));
+            
+        } else {
+            $driverDefinition->setClass('Memcached');
+            $cacheDefinition->setClass('Doctrine\Common\Cache\MemcachedCache');
+            $cacheDefinition->addMethodCall('setMemcached', array(new Reference($driverService)));
+        }
+    }
+
+    private function configureArrayCache(array $cacheConfig, ContainerBuilder $container)
+    {
+        $name = $cacheConfig['name'];
+        $cacheService = 'cache.' . $name;
+
+        $definition = $container->register($cacheService);
+        $definition->setClass('Doctrine\Common\Cache\ArrayCache');
+        $definition->addMethodCall('setNamespace', array($cacheConfig['namespace']));
     }
 }
