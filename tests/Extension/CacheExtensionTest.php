eff10a96187bcc5bda8bf4649403ef620e72cbca
Tyler Sommer <sommertm@gmail.com>
2014-04-11 09:13:34 -0600

    Added redis CacheExtension test

--- tests/Extension/CacheExtensionTest.php
+++ tests/Extension/CacheExtensionTest.php
@@ -51,6 +51,84 @@ class CacheExtensionTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Tests redis configurations
+     */
+    public function testRedisConfig()
+    {
+        $extension = new CacheExtension();
+
+        $container = new ContainerBuilder();
+        $extension->load(array(array(
+                'connections' => array(
+                    'default' => array(
+                        'driver' => 'redis',
+                        'namespace' => 'test:',
+                        'options' => array(
+                            'host' => '10.0.0.155',
+                            'port' => '16379',
+                            'timeout' => 30,
+                            'persistent' => false
+                        )
+                    ),
+                    'secondary' => array(
+                        'driver' => 'redis',
+                        'options' => array(
+                            'socket' => '/tmp/redis.sock',
+                            'persistent' => true
+                        )
+                    )
+                )
+            )), $container);
+        
+        $this->assertTrue($container->hasDefinition('cache.default'));
+        $this->assertTrue($container->hasDefinition('cache.default.driver'));
+        $this->assertTrue($container->hasDefinition('cache.secondary'));
+        $this->assertTrue($container->hasDefinition('cache.secondary.driver'));
+        
+        $driverDefinition = $container->getDefinition('cache.default.driver');
+        $methodCalls = $driverDefinition->getMethodCalls();
+        $methodName = $methodCalls[0][0];
+        $methodArgs = $methodCalls[0][1];
+        $this->assertEquals('Redis', $driverDefinition->getClass());
+        $this->assertEquals('connect', $methodName);
+        $this->assertEquals('10.0.0.155', $methodArgs[0]);
+        $this->assertEquals('16379', $methodArgs[1]);
+        $this->assertEquals(30, $methodArgs[2]);
+        
+        $cacheDefinition = $container->getDefinition('cache.default');
+        $methodCalls = $cacheDefinition->getMethodCalls();
+        $methodName = $methodCalls[0][0];
+        $methodArgs = $methodCalls[0][1];
+        $this->assertEquals('setRedis', $methodName);
+        $this->assertEquals('cache.default.driver', $methodArgs[0]);
+        $methodName = $methodCalls[1][0];
+        $methodArgs = $methodCalls[1][1];
+        $this->assertEquals('setNamespace', $methodName);
+        $this->assertEquals('test:', $methodArgs[0]);
+
+        $driverDefinition = $container->getDefinition('cache.secondary.driver');
+        $methodCalls = $driverDefinition->getMethodCalls();
+        $methodName = $methodCalls[0][0];
+        $methodArgs = $methodCalls[0][1];
+        $this->assertEquals('Redis', $driverDefinition->getClass());
+        $this->assertEquals('pconnect', $methodName);
+        $this->assertEquals('/tmp/redis.sock', $methodArgs[0]);
+        $this->assertEquals(null, $methodArgs[1]);
+        $this->assertEquals(0, $methodArgs[2]);
+
+        $cacheDefinition = $container->getDefinition('cache.secondary');
+        $methodCalls = $cacheDefinition->getMethodCalls();
+        $methodName = $methodCalls[0][0];
+        $methodArgs = $methodCalls[0][1];
+        $this->assertEquals('setRedis', $methodName);
+        $this->assertEquals('cache.secondary.driver', $methodArgs[0]);
+        $methodName = $methodCalls[1][0];
+        $methodArgs = $methodCalls[1][1];
+        $this->assertEquals('setNamespace', $methodName);
+        $this->assertEquals('nice:', $methodArgs[0]);
+    }
+    
+    /**
      * Test the getConfiguration method
      */
     public function testGetConfiguration()
